05b690141fc62ac1456640366c977fbfffec142a
James Newman <jamesmnewman1@gmail.com>
2015-08-31 20:03:41 -0700

    scope and more complex directives

--- directives/directive_ex.js
+++ directives/directive_ex.js
@@ -20,6 +20,59 @@ angular.module('functionalities', [])
 		})
 	 }
 })
+.directive("welcome", function() {
+	return {
+		restrict: "E",
+		controller: function($scope) {
+			$scope.words = [];
+
+			this.sayHello = function() {
+				$scope.words.push("hello");
+			};
+
+			this.sayHowdy = function() {
+				$scope.words.push("WOES!!");
+			};
+
+			this.sayHi = function() {
+				$scope.words.push("Hi!")
+			};
+		},
+		link: function(scope, element) {
+			element.bind("mouseenter", function() {
+				console.log(scope.words);
+			});
+		}
+	}
+})
+.directive("hello", function() {
+	return {
+		require: "welcome",
+		link: function(scope, element, attrs, welcomeCtrl) {
+			welcomeCtrl.sayHowdy();
+		}
+	};
+})
+
+.directive("howdy", function() {
+  return {
+    require: "welcome",
+    link: function (scope, element, attrs, welcomeCtrl) {
+      welcomeCtrl.sayHowdy();
+    }
+  };
+})
+
+.directive("hi", function() {
+  return {
+    require: "welcome",
+    link: function (scope, element, attrs, welcomeCtrl) {
+      welcomeCtrl.sayHi();
+    }
+  };
+});
+
+
 
 // .directive("leaving", function() {
 // 	return function(scope, element) {
