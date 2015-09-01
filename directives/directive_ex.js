b3284f5cb92d1579b1b8f5c5fd01c4aee9c8e0fa
James Newman <jamesmnewman1@gmail.com>
2015-08-31 19:41:51 -0700

    controllers + directives

--- directives/directive_ex.js
+++ directives/directive_ex.js
@@ -1,18 +1,31 @@
+function exampleCtrl() {
+	this.start = function() {
+		console.log("fun times have begun!")
+	}
+	this.end = function() {
+		console.log("fun is no longer had....")
+	}
+}
+
 angular.module('functionalities', [])
+.controller('exampleCtrl', exampleCtrl)
 .directive("entering", function() {
 	return function(scope, element, attrs) {
 		element.bind("mouseenter", function() {
-			element.addClass(attrs.entering);
+			// $scope.apply() parses the passed string and finds the method within 
+			// the scope. 
+			scope.$apply("example.start()");
+			scope.$apply("example.end()");
 			console.log("mouse is in the div");
 		})
 	 }
 })
 
-.directive("leaving", function() {
-	return function(scope, element) {
-		element.bind("mouseleave", function(){
-			element.removeClass(attrs.entering);
-			console.log("mouse has left the div");
-		})
-	}
-});
+// .directive("leaving", function() {
+// 	return function(scope, element) {
+// 		element.bind("mouseleave", function(){
+// 			element.removeClass(attrs.entering);
+// 			console.log("mouse has left the div");
+// 		})
+// 	}
+// });
